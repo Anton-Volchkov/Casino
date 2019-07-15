92d352a724be19a558f8dd7d7eecdae80e3fed14
Anton-Volchkov <anton.volchkov.2001@gmail.com>
2019-07-15 22:28:21 +0300

    Залил файлы проекта.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button5
+            // 
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button5.Location = new System.Drawing.Point(3, 508);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(109, 46);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Выход";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(229, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(499, 380);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(0, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(75, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 38);
+            this.label2.TabIndex = 8;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(381, 146);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 100);
+            this.panel2.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(290, 405);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(113, 22);
+            this.textBox1.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(226, 405);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Ставка:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(358, 505);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(223, 41);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Играть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel1.Location = new System.Drawing.Point(111, -24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1, 671);
+            this.panel1.TabIndex = 13;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(226, 433);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 17);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Число";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(290, 433);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(113, 22);
+            this.textBox2.TabIndex = 14;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox1.Location = new System.Drawing.Point(621, 398);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(107, 109);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Цвет";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 75);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(86, 21);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Зеленое";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 48);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 21);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Красное";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 21);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(79, 21);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Черное";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(39)))), ((int)(((byte)(41)))));
+            this.ClientSize = new System.Drawing.Size(810, 558);
+            this.ControlBox = false;
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button5);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            balance = 500;
+            SetBalance();
+            
+        }
+        
+        int balance;
+        Colors userColors;
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+           
+            Application.Exit();
+        }
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            if ( string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Вы не  указали сумму ставки!");
+                return;
+            }
+            if (Int32.TryParse(textBox1.Text, out int result))
+            {
+                if (result < 1 )
+                {
+                    MessageBox.Show("Некорректная ставка!");
+                    return;
+                }
+                if (balance - result >= 0)
+                {
+
+                    if (radioButton1.Checked)
+                    {
+                        userColors = Colors.Black;
+                    }
+                    else if (radioButton2.Checked)
+                    {
+                        userColors = Colors.Red;
+                    }
+                    else if (radioButton3.Checked)
+                    {
+                        userColors = Colors.Green;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Выберите цвет!");
+                        return;
+                    }
+
+                    SetBoolean(false);
+
+                    balance -= result;
+                    SetBalance();
+                    timer1.Enabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("У вас недостаточно средств!");
+                }
+
+            }
+        }
+
+        private void SetBoolean(bool setbool)
+        {
+            textBox1.Enabled = setbool;
+            button1.Enabled = setbool;
+            groupBox1.Enabled = setbool;
+            textBox2.Enabled = setbool;
+        }
+
+        public void SetBalance()
+        {
+            label1.Text = $"Баланс: {balance}$";
+        }
+
+        int StopRoll = 0;
+        public void Roll(Colors userColor)
+        {
+            pictureBox1.Image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+            pictureBox1.Refresh();
+
+            Colors cl = Colors.Green;
+            Random rnd = new Random();
+            int roll = rnd.Next(0, 36);
+            label2.Text = roll.ToString();
+
+            if (Black.Contains(roll))
+            {
+                panel2.BackColor = Color.Black;
+                cl = Colors.Black;
+            }
+            else if (Rad.Contains(roll))
+            {
+                panel2.BackColor = Color.Red;
+                cl = Colors.Red;
+            }
+            else if (roll == 0)
+            {
+                panel2.BackColor = Color.Green;
+                cl = Colors.Green;
+            }
+            StopRoll++;
+
+            if (StopRoll == 50)
+            {
+                StopRoll = 0;
+                timer1.Enabled = false;
+                int coef = 0;
+                if (string.IsNullOrWhiteSpace(textBox2.Text) && cl == userColor)
+                {
+                    coef = 2;
+                }
+                else if (cl == userColor && roll == Convert.ToInt32(textBox2.Text) )
+                {
+                    coef = cl == Colors.Green ? 12 : 4;
+                }
+                else
+                {
+                    MessageBox.Show("Вы проиграли!");
+                    SetBoolean(true);
+                    return;
+                }
+
+                balance += Convert.ToInt32(textBox1.Text) * coef;
+                MessageBox.Show("Поздравляем с победой!");
+                SetBalance();
+                SetBoolean(true);
+            }
+            
+            
+        }
+
+        public int[] Black { get; set; } = {10,29,8,31,6,33,4,35,2,28,26,11,20,17,22,15,24,13 };
+        public int[] Rad { get; set; } = { 27,35,12,19,18,21,16,23,14,6,30,7,32,5,34,3,36,1 };
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            Roll(userColors);
+        }
+    }
+}
